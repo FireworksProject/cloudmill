5935a7a41eb20b5eeb7a0b16da8d2301c3f25ead
Brian Rowe <briprowe@yahoo.com>
2012-10-04 04:46:32 -0400

    Refactor bootstrap.sh a little.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,44 @@
 
 cd `dirname $0`
 
-vboxwebsrv -t0
-VBoxManage setproperty websrvauthlibrary null
+PALLET_CONF_FILE="~/.pallet/config.clj"
+DEFAULT_PALLET_CONF="./resources/pallet-config.clj"
 
-if ! [ -e ~/.pallet/config.clj ]; then
-    cp resources/pallet-config.clj ~/.pallet/config.clj
+function fail {
+    echo "$@" >&2
+    exit 1
+}
+
+function warn {
+    echo "$@" >&2
+}
+
+function ensure_dir {
+    if ! [ -d "$1" ]; then
+        mkdir -p -- "$1" || fail "couldn't create $1"
+    fi
+}
+
+function ensure_available {
+    if [ -z `which $1` ]; then
+        fail "ERROR:\tCould not find $1 on your path.\n\t$2"
+        exit 1
+    fi
+}
+
+ensure_available "lein"       "Ensure you have leiningen installed: https://github.com/technomancy/leiningen"
+ensure_available "vboxwebsrv" "Ensure you have installed VirtualBox"
+ensure_available "VBoxManage" "Ensure you have installed VirtualBox"
+
+if ! [ -e $PALLET_CONF_FILE ]; then
+    ensure_dir `dirname $PALLET_CONF_FILE`
+    cp $DEFAULT_PALLET_CONF $PALLET_CONF_FILE  || warn "Couldn't copy $DEFAULT_PALLET_CONF to $PALLET_CONF_FILE"
 fi
+
+if ! [ -e ~/.ssh/id_rsa ]; then
+    ssh-keygen -t rsa
+fi
+
+ssh-add -K ~/.ssh/id_rsa || fail "Could not add ssh key to ssh-agent"
+vboxwebsrv -t0 || fail "Failure invoking: vboxwebsrv -t0"
+VBoxManage setproperty null || fail "Failure invoking: VBoxManage setproperty websrvauthlibrary null"
