32835bf0101c22b2ab2a542b8085e1e386588a1c
Brian Rowe <briprowe@yahoo.com>
2012-10-04 03:13:32 -0400

    Modify the bootstrap script to ensure that pallet has a vmfest compute provider.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
 
+cd `dirname $0`
+
 vboxwebsrv -t0
 VBoxManage setproperty websrvauthlibrary null
+
+if ! [ -e ~/.pallet/config.clj ]; then
+    cp resources/pallet-config.clj ~/.pallet/config.clj
+fi
---
+++ resources/pallet-config.clj
@@ -0,0 +1,4 @@
+(defpallet :services {:vmfest {:provider "vmfest"
+                               :default-network-type :local
+                               :default-local-interface "vboxnet0"
+                               :default-bridged-interface "en1: Wi-Fi (AirPort)"}})
