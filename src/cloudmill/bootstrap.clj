e52b8d47a7c4bd9034cae092c3f29cfda0c16856
Brian Rowe <briprowe@yahoo.com>
2013-01-24 13:20:12 -0500

    Improve output when downloading new VM base-images.

--- src/cloudmill/bootstrap.clj
+++ src/cloudmill/bootstrap.clj
@@ -7,34 +7,12 @@
             [clojure.java.io :as io]
             [pallet.compute :as compute]
             [pallet.configure :refer [compute-service]]
-            [pallet.compute.vmfest :as vmfest]
             [clojure.tools.logging :as l]
 
             cloudmill.repl))
 
 (def bootstrap-sh (io/resource "bootstrap.sh"))
 (def pallet-config (io/resource "pallet-config.clj"))
-
-(def debian-vdi-url "https://s3.amazonaws.com/vmfest-images/debian-6.0.2.1-64bit-v0.3.vdi.gz")
-
-(defn add-image
-  "Wrap pallet.compute.vmfest/add-image so it has nicer semantics.
-
-   Return true if image was successfully added (or has previously
-   been added), false otherwise."
-  [image]
-  (try (nil? (vmfest/add-image (compute-service :virtualbox) image))
-       (catch clojure.lang.ExceptionInfo _
-         false)))
-
-(defn attempt-to-add-image
-  "Loops for up to timeout milliseconds while trying to connect to the vboxwebsrv process.
-   Once connected, download and add a .vdi if needed."
-  [timeout image]
-  (let [start-time (System/currentTimeMillis)]
-    (loop [current-time (System/currentTimeMillis), added? false]
-      (when (and (not added?) (< (- current-time start-time) timeout))
-        (recur (System/currentTimeMillis) (add-image image))))))
 
 (defn bootstrap
   [logfile]
@@ -55,7 +33,6 @@
     (future (sh/stream-to proc :err (io/as-file (str logfile ".err.log"))))
 
     (println "Starting virtualbox...")
-    (attempt-to-add-image 1000 debian-vdi-url)
     (fn []
       (doto proc sh/destroy sh/exit-code)
       (sh/exit-code (sh/proc "killall" "vboxwebsrv")))))
--- src/cloudmill/virtualbox.clj
+++ src/cloudmill/virtualbox.clj
@@ -7,24 +7,65 @@
             [pallet.node :refer [primary-ip ssh-port]]
             [pallet.crate.automated-admin-user :refer [automated-admin-user]]
             [pallet.actions :refer [exec-script]]
+            [pallet.compute.vmfest :as vmfest]
             [pallet.configure :refer [compute-service]]))
 
-(def default-group (group-spec "debian-vm"
-                               :phases {:bootstrap (plan-fn (automated-admin-user))}
-                               :node-spec (node-spec :image {:os-family :debian})))
+(def groups {"debian" {:group-spec (group-spec
+                                    "debian-vm"
+                                    :phases {:bootstrap (plan-fn (automated-admin-user))}
+                                    :node-spec (node-spec :image {:os-family :debian}))
+                       :image-url "https://s3.amazonaws.com/vmfest-images/debian-6.0.2.1-64bit-v0.3.vdi.gz"}})
+
+(defn add-image
+  "Wrap pallet.compute.vmfest/add-image so it has nicer semantics.
+
+   Return true if image was successfully added (or has previously
+   been added), false otherwise."
+  [image]
+  (try (nil? (vmfest/add-image (compute-service :virtualbox) image))
+       (catch clojure.lang.ExceptionInfo _
+         false)))
+
+(defn attempt-to-add-image
+  "Loops for up to timeout milliseconds while trying to connect to the vboxwebsrv process.
+   Once connected, download and add a .vdi if needed."
+  [timeout image]
+  (let [start-time (System/currentTimeMillis)]
+    (loop [current-time (System/currentTimeMillis), added? false]
+      (when (and (not added?) (< (- current-time start-time) timeout))
+        (recur (System/currentTimeMillis) (add-image image))))))
+
+(defn ensure-image
+  [image-spec image-url]
+  (when (empty? (vmfest/find-images (compute-service :virtualbox) image-spec))
+    (printf "Downdloading image: %s\n" image-url)
+    (.flush *out*)
+    (let [add-image-future (future (add-image image-url))
+          timeout 3000, line-width 80]
+      (loop [done-sentinel (deref add-image-future timeout :not-done)
+             x-pos 1]
+        (when (= done-sentinel :not-done)
+          (if (zero? (mod x-pos line-width))
+            (print "\n")
+            (print "."))
+          (.flush *out*)
+          (recur (deref add-image-future timeout :not-done) (inc x-pos)))))
+    (println "")))
 
 (defn start-vm
-  [& args]
-  (println "starting vm...")
-  (-> @(converge {default-group 1} :compute (compute-service :virtualbox))
-      :new-nodes
-      first
-      :node))
+  [name & args]
+  (let [group (get-in groups [name :group-spec])]
+    (ensure-image (:image group) (get-in groups [name :image-url]))
+    (println "starting vm...")
+    (-> @(converge {group 1} :compute (compute-service :virtualbox))
+        :new-nodes
+        first
+        :node)))
 
 (defn stop-vm
-  [& args]
+  [name & args]
   (println "stopping vm...")
-  @(converge {default-group 0} :compute (compute-service :virtualbox))
+  @(converge {(get-in groups [name :group-spec]) 0} :compute (compute-service :virtualbox))
   (println "done."))
 
 (defn output-results
@@ -38,12 +79,13 @@
 Understood operations are:
   start
   stop"
-  [command & args]
-  (let [stop-vboxweb (bootstrap "logs/vboxweb")]
+  [command & [name args]]
+  (let [stop-vboxweb (bootstrap "logs/vboxweb")
+        name (or name "debian")]
     (-> (Runtime/getRuntime) (.addShutdownHook (Thread. stop-vboxweb)))
     
     (case command
-      "start" (output-results (start-vm))
-      "stop"  (stop-vm))))
+      "start" (output-results (start-vm name))
+      "stop"  (stop-vm name))))
 
 (dsp/register-command "vbox" #'virtualbox)
